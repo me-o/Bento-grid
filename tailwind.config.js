e7bafc4db98bfa11a87a35817d446ad8f205d965
c_7 <caoqi_1019@qq.com>
2024-09-14 10:58:42 +0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				DMSans: ['"DM Sans"', 'sans-serif'],
+			},
+			colors: {
+				'purple-100': 'hsl(254, 88%, 90%)',
+				'purple-500': 'hsl(256, 67%, 59%)',
+				'yellow-100': 'hsl(31, 66%, 93%)',
+				'yellow-500': 'hsl(39, 100%, 71%)',
+				black: 'hsl(0, 0%, 7%)',
+			},
+			screens: {
+				mobile: '375px',
+				desktop: '1440px',
+			},
+			minWidth: {
+				mobile: '375px',
+				desktop: '1440px',
+			},
+		},
+	},
+	plugins: [],
+};
